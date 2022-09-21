cd19b1281b541ec2ae8a69ddd7640d353463fb4f
niris <niris@users.noreply.github.com>
2022-09-22 00:08:06 +0700

    modify welcome message

--- static/BotOnline.js
+++ static/BotOnline.js
@@ -9,7 +9,7 @@ const welcomeMessage = `How can I help you?
 const BotOnlineChat = {
     data: () => ({ logs: [] , greetingTimeout:0, suggestions:["English", "ภาษาไทย"], encourageMsg:["Bravo!","Good job","Well done!"], mode:"normal", welcomeMsg:''}),
     created () {
-        this.logs.push({ bot: true, msg: "Welcome!" });
+        this.logs.push({ bot: true, msg: "Welcome !"});
         this.greetingTimeout = setTimeout(() => this.logs=[], 5000);
     },
     computed: {
@@ -45,6 +45,9 @@ const BotOnlineChat = {
         });
         },
         async send({ target }) {  
+            this.welcomeMessage=[];
+            this.mode = "normal";
+            clearTimeout(this.greetingTimeout)
             const msg = target.req.value;
             target.req.value = '';
 
@@ -124,7 +127,7 @@ const BotOnlineChat = {
         async classify(text, language_code) {
             const res = await (await fetch(`/dialog?${new URLSearchParams({text, language_code})}`)).json()
             console.log(res, res.intent)
-            let resMessage = ["OK, let's go!", "Let's go!!!!!"]
+            let resMessage = ["OK, let's go!", "Let's go!"]
             let randomMessage = resMessage[Math.floor(Math.random() * resMessage.length)]
             switch (res.intent) {
                 case "Vocab":
@@ -152,10 +155,10 @@ const BotOnlineChat = {
     template: `
     <form class=chatbot @submit.prevent=send>
         <output v-if="mode=='welcome'" class="card bot bg-primary text-white" v-html="md(welcomeMsg)" @click="minimize"></output>
-        <output v-for="log in last_logs" :class="'card '+(log.bot?'bot bg-primary text-white':'user')" v-html="md(log.msg)"></output>
+        <output v-for="log in last_logs" :class="'card '+(log.bot?'bot bg-primary text-white':'user')" v-html="md(log.msg)" @click="minimize"></output>
         <details class=field>
             <summary>
-            <svg class=bot xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 492 492">
+            <svg class=bot xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 492 492" @click=welcome>
                 <path style="opacity:.7" d="m 119,177 c 0,-11 26,-20 37,-23 24,-7 54,-11 83,-11 29,0 59,4 83,11 11,3 37,12 37,23 l -0,19 63,-25 c 3,-1 5,-3 5,-5 0,-1 -1,-4 -4,-5 L 255,68 c -8,-4 -22,-4 -30,0 L 55,160 c -3,1 -5,3 -4,5 0,1 2,3 5,5 l 63,25 z m 0,0"/>
                 <path style="opacity:.7" d="M 241,151 A 138,137 0 0 0 102,289 138,137 0 0 0 241,426 138,137 0 0 0 379,289 138,137 0 0 0 241,151 Z m 54,117 a 22,22 0 0 1 22,22 22,22 0 0 1 -22,22 22,22 0 0 1 -22,-22 22,22 0 0 1 22,-22 z m -107,0 a 22,22 0 0 1 22,22 22,22 0 0 1 -22,22 22,22 0 0 1 -22,-22 22,22 0 0 1 22,-22 z" />
                 <path style="opacity:.9" d="m 129,225 c -37,0 -68,30 -68,68 0,37 30,68 68,68 h 211 c 37,0 68,-30 68,-68 0,-37 -30,-68 -68,-68 z m 165,43 a 22,22 0 0 1 22,22 22,22 0 0 1 -22,22 22,22 0 0 1 -22,-22 22,22 0 0 1 22,-22 z m -108,0 a 22,22 0 0 1 22,22 22,22 0 0 1 -22,22 22,22 0 0 1 -22,-22 22,22 0 0 1 22,-22 z" />
@@ -164,7 +167,7 @@ const BotOnlineChat = {
             <nav>
                 <input type=button v-if="logs.length" @click.prevent="logs=[]" class="button icon-only picon" value=times>
                 <input type=button @click="listen({target:$refs.req})" class="button icon-only picon" value=microphone>
-                <input name="req" ref=req placeholder="Question" autocomplete="off" id="msgfeild" @focus.prevent=welcome @input=minimize>
+                <input name="req" ref=req placeholder="Question" autocomplete="off" id="msgfeild" @input=minimize @focus="minimize">
                 <button class="button icon-only picon">send</button>
             </nav>
         </details>
